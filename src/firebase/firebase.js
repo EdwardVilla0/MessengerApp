e86afe80c26f7d546b27c65340a0ba4b4f82bb88
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-03-08 15:27:13 -0800

    cleaning up my project for unwanted items

---
+++ src/firebase/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyATPEjf-MzX3uAAU-hFRiqFtQD0p6dZWTA",
+    authDomain: "messenger-app-9f755.firebaseapp.com",
+    projectId: "messenger-app-9f755",
+    storageBucket: "messenger-app-9f755.appspot.com",
+    messagingSenderId: "122405109717",
+    appId: "1:122405109717:web:75a279667579e9e57a875c",
+    measurementId: "G-D6Y9W75087"
+};
