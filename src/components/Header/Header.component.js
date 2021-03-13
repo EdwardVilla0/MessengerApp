aee0be8102f7fd94cc1a242bfaa3a85a8f56b6ac
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-03-12 18:05:24 -0800

    fixing the styling for the header

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -30,7 +30,12 @@ export default Header;
 const HeaderRight = styled.div`
 flex: 0.3;
 display: flex;
-align-items: center;
+align-items: flex-end;
+
+> .MuiSvgIcon-root{
+    margin-left: auto;
+    margin-right: 20px;
+}
 `
 
 const HeaderSearch = styled.div`
