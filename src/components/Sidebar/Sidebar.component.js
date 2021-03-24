5569cb8d58381cd90a8eac826d33c0dc55cb74c2
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-03-23 19:16:39 -0700

    setting up options in the sidebar

--- src/components/Sidebar/Sidebar.component.js
+++ src/components/Sidebar/Sidebar.component.js
@@ -2,6 +2,16 @@ import React from 'react'
 import styled from "styled-components";
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
 import CreateIcon from '@material-ui/icons/Create'
+import SidebarOption from './SidebarOption/SidebarOption.component';
+import InsertCommentIcon from '@material-ui/icons/InsertComment'
+import InboxIcon from '@material-ui/icons/Inbox'
+import DraftsIcon from '@material-ui/icons/Drafts'
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt'
+import AppsIcon from '@material-ui/icons/Apps'
+import FileCopyIcon from '@material-ui/icons/FileCopy'
+import ExpandLessIcon from '@material-ui/icons/ExpandLess'
+
 
 
 function Sidebar() {
@@ -18,6 +28,14 @@ function Sidebar() {
                     </SidebarInfo>
                     <CreateIcon />
                 </SidebarHeader>
+                <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+                <SidebarOption Icon={InboxIcon} title="Mentions & reactions" />
+                <SidebarOption Icon={DraftsIcon} title="Saved items" />
+                <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser" />
+                <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+                <SidebarOption Icon={AppsIcon} title="Apps" />
+                <SidebarOption Icon={FileCopyIcon} title="File Browser" />
+                <SidebarOption Icon={ExpandLessIcon} title="Show less" />
             </SidebarContainer>
         </div>
     )
--- src/components/Sidebar/SidebarOption/SidebarOption.component.js
+++ src/components/Sidebar/SidebarOption/SidebarOption.component.js
@@ -1,13 +1,13 @@
-import { styled } from '@material-ui/core';
+import styled from 'styled-components';
 import React from 'react'
 
 
-function SidebarOption() {
+function SidebarOption({ Icon, title }) {
     return (
         <SidebarOptionContainer></SidebarOptionContainer>
     )
 }
 
-export default SidebarOption;
+export default SidebarOption
 
-const SidebarOptionContainer = styled.div``
+const SidebarOptionContainer = styled.div``;
