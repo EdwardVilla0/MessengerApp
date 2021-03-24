e594f3f4af091dfbac11e96d338f880505792528
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-03-24 14:53:38 -0700

    styling sidebar option

--- src/components/Sidebar/SidebarOption/SidebarOption.component.js
+++ src/components/Sidebar/SidebarOption/SidebarOption.component.js
@@ -4,10 +4,25 @@ import React from 'react'
 
 function SidebarOption({ Icon, title }) {
     return (
-        <SidebarOptionContainer></SidebarOptionContainer>
+        <SidebarOptionContainer>
+            {Icon && <Icon fontSize='small' style={{ padding: 10 }} />}
+            {Icon ? (
+                <h3>{title}</h3>
+            ) : (<SidebarOptionChannel>
+                <span>#</span> {title}
+            </SidebarOptionChannel>)}
+        </SidebarOptionContainer>
     )
 }
 
 export default SidebarOption
 
-const SidebarOptionContainer = styled.div``;
+const SidebarOptionContainer = styled.div`
+    display: flex;
+    font-size: 12px;
+    align-items: center;
+    padding-left: 2px;
+    cursor: pointer;
+`;
+
+const SidebarOptionChannel = styled.div``;
