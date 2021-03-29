fc927b79427b0c823e8336d28ed15804030f7462
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-03-29 12:53:51 -0700

    installing firebase hooks to project

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyATPEjf-MzX3uAAU-hFRiqFtQD0p6dZWTA",
     authDomain: "messenger-app-9f755.firebaseapp.com",
@@ -8,3 +10,10 @@ const firebaseConfig = {
     appId: "1:122405109717:web:75a279667579e9e57a875c",
     measurementId: "G-D6Y9W75087"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
