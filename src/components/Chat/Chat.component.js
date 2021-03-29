d6b52a9227a901c157ca74fd1344a30d0deef2cd
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-03-29 13:47:44 -0700

    styling the chat component

--- src/components/Chat/Chat.component.js
+++ src/components/Chat/Chat.component.js
@@ -1,26 +1,38 @@
 import styled from 'styled-components'
 import React from 'react'
+import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined'
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined'
 
 function Chat() {
     return (
         <ChatContainer>
-            <h1>I am the chat screen</h1>
-            <h1>I am the chat screen</h1>
-            <h1>I am the chat screen</h1>
-            <h1>I am the chat screen</h1>
-            <h1>I am the chat screen</h1>
-            <h1>I am the chat screen</h1>
-            <h1>I am the chat screen</h1>
-            <h1>I am the chat screen</h1>
-            <h1>I am the chat screen</h1>
+            <Header>
+                <HeaderLeft>
+                    <h4><strong>#Room-name</strong></h4>
+                    <StarBorderOutlinedIcon />
+                </HeaderLeft>
+                <HeaderRight>
+                    <p><InfoOutlinedIcon />Details</p>
+                </HeaderRight>
+            </Header>
         </ChatContainer>
     )
 }
 
 export default Chat
 
+const Header = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+    border-bottom: 1px solid lightgray;
+`
+const HeaderLeft = styled.div``
+const HeaderRight = styled.div``
+
 const ChatContainer = styled.div`
     flex: 0.7;
     flex-grow: 1;
     overflow-y: scroll;
+    margin-top: 60px;
 `
