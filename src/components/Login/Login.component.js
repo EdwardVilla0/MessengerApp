9944afa9179b63b33989faa45869a58a7c2c8343
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-03-31 17:13:19 -0700

    logging in with google functionality

--- src/components/Login/Login.component.js
+++ src/components/Login/Login.component.js
@@ -1,11 +1,13 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
 import styled from "styled-components";
+import { auth, provider } from '../../firebase/firebase';
 
 
 function Login() {
     const signIn = (e) => {
-        e.preventDefault()
+        e.preventDefault();
+        auth.signInWithPopup(provider).catch((error) => alert(error.message));
     }
     return (
         <LoginContainer>
